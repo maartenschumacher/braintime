82c45d1f82670d0966eb90490c65a9a65c6dd6f9
Maarten Schumacher <maartenschumacher@Maartens-iMac.fritz.box>
2014-12-31 16:51:07 +0100

    Initial Commit

---
+++ BrainTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BrainTime
+//
+//  Created by Maarten Schumacher on 31/12/14.
+//  Copyright (c) 2014 Maarten Schumacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BrainTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BrainTime
+//
+//  Created by Maarten Schumacher on 31/12/14.
+//  Copyright (c) 2014 Maarten Schumacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
